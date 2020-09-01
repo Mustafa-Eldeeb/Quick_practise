63dae3d1e86f3907a5edaf0c3cd5c47c7f291bca
Mustafa-Eldeeb <darshm466@gmail.com>
2020-09-01 19:21:15 +0200

    updated_program_and_Readme_file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Quick_practise
+
+To run any program:
+
+$ g++ -std=c++17 main.cpp
+$ ./a.out
+
+
+To run any multi-threaded program:
+
+$ g++ -std=c++17 main.cpp -pthread
+$ ./a.out
--- concurrency/main_func_and_sec_threads.cpp
+++ concurrency/main_func_and_sec_threads.cpp
@@ -5,18 +5,20 @@ void threadFunction()
 {
     // to simulate work 
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
-    std::cout<<"Finished work in thread"<<"\n";
+    std::cout<<"Finished work in thread\n"<<std::endl;
 }
 
 int main()
 {
     std::thread t(threadFunction);
+
     // to simulate work
     std::this_thread::sleep_for(std::chrono::microseconds(50));
     std::cout<<"Finished work in main function \n"<<std::endl;
 
     // wait thread to finish
     t.join();
+
     
     return 0;
 }
