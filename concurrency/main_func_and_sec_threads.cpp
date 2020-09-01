a8361725da8d6e2e49e694ad00066e7008f18b81
Mustafa-Eldeeb <darshm466@gmail.com>
2020-09-01 19:07:04 +0200

    complete_main_func_and_thread_func

--- concurrency/main_func_and_sec_threads.cpp
+++ concurrency/main_func_and_sec_threads.cpp
@@ -5,5 +5,18 @@ void threadFunction()
 {
     // to simulate work 
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::cout<<"Finished work in thread"<<"\n";
+}
+
+int main()
+{
+    std::thread t(threadFunction);
+    // to simulate work
+    std::this_thread::sleep_for(std::chrono::microseconds(50));
+    std::cout<<"Finished work in main function \n"<<std::endl;
+
+    // wait thread to finish
+    t.join();
     
+    return 0;
 }
