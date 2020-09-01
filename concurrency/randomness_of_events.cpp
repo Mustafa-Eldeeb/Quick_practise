c182bfa0da0a26f62363315584c48646b10a3b8f
Mustafa-Eldeeb <darshm466@gmail.com>
2020-09-01 19:40:09 +0200

    adding two  work simulation in both main and threadFunction

---
+++ concurrency/randomness_of_events.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <thread>
+
+void threadFunction()
+{
+    //simulate work 1
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::cout<<"Finshed work 1 in thread \n"<<std::endl;
+
+    //simulate work 2
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::cout<<"Finished work 2 in thread\n"<<std::endl;
+
+}
+
+int main()
+{
+    std::thread t(threadFunction);
+
+    //simulate work 1 in main
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::cout<<"Finished work 1 in main \n"<<std::endl;
+
+    //simulate work 2 in main
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    std::cout<<"finished work 2 in main\n"<<std::endl;
+
+
+    return 0;
+}
